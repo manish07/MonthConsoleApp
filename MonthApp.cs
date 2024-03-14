b9a2a6aa3ee9bea5642afe78b06fe6ad8709ff91
manish07 <manish.nmamit@gmail.com>
2024-03-14 17:35:26 +0530

    Modified message

--- MonthApp.cs
+++ MonthApp.cs
@@ -1,12 +1,12 @@
 class MonthApp {
     public static void MonthDisplayApp(string userResponse) {
         string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
-
+        Console.WriteLine($"User input: {userResponse}");
         if(string.IsNullOrWhiteSpace(userResponse)){
             Console.WriteLine("Please enter a valid month.");
             return;
         }
-
+        
         if (userResponse.Equals("All", StringComparison.OrdinalIgnoreCase)) {
             foreach(var month in months) {
                 Console.WriteLine(month);
