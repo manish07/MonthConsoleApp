bd037e2446d21baea91fa3f97c56ff22c65a2216
manish07 <manish.nmamit@gmail.com>
2024-03-14 17:30:15 +0530

    month app

---
+++ MonthApp.cs
@@ -0,0 +1,31 @@
+class MonthApp {
+    public static void MonthDisplayApp(string userResponse) {
+        string[] months = { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" };
+
+        if(string.IsNullOrWhiteSpace(userResponse)){
+            Console.WriteLine("Please enter a valid month.");
+            return;
+        }
+
+        if (userResponse.Equals("All", StringComparison.OrdinalIgnoreCase)) {
+            foreach(var month in months) {
+                Console.WriteLine(month);
+            }
+        }
+        else {
+            int.TryParse(userResponse, out int userInput);
+
+            if (userInput >=1 && userInput <= 12) {
+                Console.WriteLine($"{userInput} is {months[userInput - 1]}");
+            }
+            else {
+                if(userInput is int && userInput != 0) {
+                    Console.WriteLine($"{userInput} is not valid.");
+                }
+                else {
+                    Console.WriteLine($"\"{userResponse}\" user input is not valid.");
+                }
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿MonthApp.MonthDisplayApp("4");
+MonthApp.MonthDisplayApp("all");
+MonthApp.MonthDisplayApp("13");
+MonthApp.MonthDisplayApp("ALL");
+MonthApp.MonthDisplayApp("8");
+MonthApp.MonthDisplayApp("Not all");
+MonthApp.MonthDisplayApp("");
+MonthApp.MonthDisplayApp(" ");
